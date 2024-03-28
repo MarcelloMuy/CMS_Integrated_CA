e6ad111eae2dc72c8488a94448198f194aa690f9
Marcello Muylaert <87388600+MarcelloMuy@users.noreply.github.com>
2024-03-28 19:46:10 +0000

    Initial commit

---
+++ src/cms_integrated_ca/CMS_Integrated_CA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cms_integrated_ca;
+
+
+/**
+ *
+ * @author Marce
+ */
+public class CMS_Integrated_CA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+    }
+    
+}
